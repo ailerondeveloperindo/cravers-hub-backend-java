289468608b52272faee84a610c1adf554eafe842
Ahmad Farid <fdot.ahmadfarid@gmail.com>
2025-02-17 11:05:00 +0700

    Fix Customer Controller DI

---
+++ src/main/java/com/cravershub/craver_hub/configurations/AppConfig.java
@@ -0,0 +1,8 @@
+package com.cravershub.craver_hub.configurations;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
---
+++ src/main/java/com/cravershub/craver_hub/dto/DTO.java
@@ -0,0 +1,4 @@
+package com.cravershub.craver_hub.dto;
+
+public interface DTO {
+}
